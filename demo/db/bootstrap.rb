0b9c07407201f7dc6c3d6118cddecc480f16195c
Cora Sutton <cora@sutton.me>
2008-11-20 14:58:21 -0600

    Removing files that shouldn't be in git.

--- demo/db/bootstrap.rb
+++ demo/db/bootstrap.rb
@@ -1,4 +1,15 @@
+require RAILS_ROOT + '/test/factories'
+
 Bootstrapper.for :development do |b|
+  5.times do
+    Factory(:user)
+  end
+
+  Factory(:user, :login => "nate",
+                 :name => "Cora Sutton",
+                 :email => "nate@sevenwire.com",
+                 :password => "lololol",
+                 :password_confirmation => "lololol")
 end
 
 Bootstrapper.for :production do |b|
